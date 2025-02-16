e14635c193f2a3266e7f1be2a3ed42874680bf6a
spdys <noorii549@yahoo.com>
2025-02-16 20:34:29 +0300

    Finished up to step 5, made minor changes in var names for readability

--- src/library/Book.kt
+++ src/library/Book.kt
@@ -2,6 +2,6 @@ package library
 
 open class Book(var title: String, var author: String, var ISBN: Long) {
     fun displayInfo() {
-        println("$title by $author ($ISBN)")
+        println("$title by $author (Hardcover ISBN: $ISBN)")
     }
 }
--- src/library/Main.kt
+++ src/library/Main.kt
@@ -1,6 +1,12 @@
-import library.Book
+import library.FictionBook
+import library.NonFictionBook
 
 fun main() {
-    val myBook = Book("Annie Bot", "Sierra Greer", 9780063312692, "Science Fiction")
-    myBook.displayInfo()
+    val fictionBook = FictionBook("Annie Bot", "Sierra Greer", 9780063312692, "Science Fiction")
+    fictionBook.displayInfo()
+    fictionBook.displayGenre()
+
+    val nonFictionBook = NonFictionBook("The Science of Stuck", "Britt Frank", 9780593419441, "Psychology")
+    nonFictionBook.displayInfo()
+    nonFictionBook.displaySubjectArea()
 }
